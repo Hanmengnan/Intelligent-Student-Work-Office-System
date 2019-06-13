f2b9ad22a52e364e3e81336cf3ec38b8757d939a
Llym <1448189829@qq.com>
2019-06-13 10:50:20 +0800

    Beta 1.2.9 pad 方案未被放弃 Pad 加入照片

--- Pad/main.py
+++
@@ -1,8 +0,0 @@
-from Pad.client import *
-from Pad.windowStudent import *
-from Door.windowDoor import *
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    client=teacherClient()
-    client.do()
-    sys.exit(app.exec_())
--- Surface/main.py
+++
@@ -1,9 +0,0 @@
-from client import *
-from windowStudent import *
-#from Door.windowDoor import *
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    client=teacherClient()
-    client.do()
-    sys.exit(app.exec_())
-            
